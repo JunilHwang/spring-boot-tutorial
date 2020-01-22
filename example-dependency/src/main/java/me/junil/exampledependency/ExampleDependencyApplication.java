7a18d11714c8d272cac4f9d5080cb10818c3543d
junil-hwang <tjsdlf4261@naver.com>
2020-01-22 20:16:15 +0900

    의존성 예제 추가 완료

---
+++ example-dependency/src/main/java/me/junil/exampledependency/ExampleDependencyApplication.java
@@ -0,0 +1,13 @@
+package me.junil.exampledependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleDependencyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExampleDependencyApplication.class, args);
+  }
+
+}
