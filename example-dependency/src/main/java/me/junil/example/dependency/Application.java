c2509421d74904cfa523ee89632aa32cad4ee9bb
junilhwang <tjsdlf4261@naver.com>
2020-01-23 16:18:37 +0900

    example pom 이름 수정

--- example-dependency/src/main/java/me/junil/example/dependency/Application.java
+++ example-dependency/src/main/java/me/junil/example/dependency/Application.java
@@ -1,4 +1,4 @@
-package me.junil.example.internal.webapp;
+package me.junil.example.dependency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
